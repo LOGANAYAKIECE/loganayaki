d73b4e048d29c777ac5619b2699a47b0e5057b2f
LOGANAYAKIECE <loganayakiece1996@gmail.com>
2017-06-07 10:57:47 +0530

    Add files via upload

---
+++ alphabet.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+ {
+
+char c;
+printf("enter the character");
+
+scanf("%c",&c);
+
+if(c>='a' && c<='z' || c>='A' &&  c<='Z')
+
+{
+
+printf("alphabet");
+
+}
+
+else
+
+{
+    
+printf("not alphabet");
+
+}
+
+return(0);
+
+}
