96d128c0478fc402b80a3b1313766d9ad819f676
LOGANAYAKIECE <loganayakiece1996@gmail.com>
2017-06-15 12:37:30 +0530

    Add files via upload

---
+++ greater.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+void main()
+
+{
+    
+int a,b,c;
+   
+ printf("enter the values of a,b,c/n");
+  
+  scanf("%d %d %d",&a,&b,&c);
+  
+  if(a>b&&a>c)
+   
+ {
+       
+ printf("a is greater than b and c");
+  
+  }
+   
+ else if(b>c)
+    
+{
+       
+ printf("/n b is greater");
+  
+  }
+    
+else
+  
+  {
+   
+     printf("/n c is greater");
+  
+  }
+   
+ }
+ 
+   
