35cc20396b959139dbe4740ae2d40fc977f9835f
LOGANAYAKIECE <loganayakiece1996@gmail.com>
2017-06-23 10:46:58 +0530

    Add files via upload

--- hello.java
+++ hello.java
@@ -1,12 +1,17 @@
-public class HelloWorld
+package program;
+import java.io.*;
+public class Hello
 {
-
-    
- public static void main(String []args)
-{
-       
- System.out.println("Hello World");
-    
- }
-
+	public static void main(String[] args)
+	{
+		int i=0;
+		
+		{
+			for(i=0;i<5;i++)
+			{
+				System.out.println("hello");
+			}
+		}
+	}
 }
+		
