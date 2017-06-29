5470437c38f289db228bf916b15d35dea94edec8
LOGANAYAKIECE <loganayakiece1996@gmail.com>
2017-06-29 11:42:48 +0530

    Add files via upload

---
+++ odd .java
@@ -0,0 +1,30 @@
+package program;
+import java.io.*;
+import java.util.Scanner;
+public class Odd 
+{	
+	public static void main(String[] args)
+	{
+		int i;
+		for(i=1;i<=99;i++)
+		{
+			if(i%2!=0)
+			System.out.println(i);
+		}
+	}
+}
+
+		
+		
+		
+	
+		
+	
+
+	
+
+	
+	
+
+
+
